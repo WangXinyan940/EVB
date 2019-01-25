b3a749f77d7b56c17b0922a810b4246e637c7db9
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-01-25 15:49:07 +0800

    Update GA.py

--- example/GA.py
+++ example/GA.py
@@ -12,14 +12,31 @@ import evb
 
 QMDATA = "qm/"
 TEMPFILE = "conf.temp"
-VAR_SHAPE = 7
-VAR_MIN = -1e3
-VAR_MAX = 1e3
-INITSIZE = 500
-REMAIN = 300
-LOWMULT = 200
-HIGHMULT = 100
-NEWGEN = 200
+STATE_TEMPFILE = ["state_1.temp", "state_2.temp"]
+VIB_UNIT = 0.001
+LIM = 50.0
+INITSIZE = 300
+REMAIN = 150
+TOP = 20
+LOWMULT = 130
+HIGHMULT = 50
+NEWGEN = 100
+
+VAR_INIT = np.array([-35,  0.587,  0.5664, -0.0232886395,
+                     2.4108,  1.7586,  0.10749,  167845.344,
+                     0.1813,  77152.96,  0.010051,  115.705145,
+                     0.0283,  112.2809,  8.359835,  13.6440055,
+                     0.1076,  162364.304,  0.1900658,  59835.384,
+                     0.0624,  94.8828612,  0.1157697,  99.824709,
+                     8.4451,  13.6449845])
+
+VAR_LIMIT = ((-4.5e1, 0.000), (0.000, 2.000), (0.000, 2.000), (-1.000, 1.000),
+             (0.000, 5.000), (0.000, 10.000), (0.080, 0.120), (1.4e5, 1.9e5),
+             (0.140, 0.220), (5.0e4, 8.5e4), (0.005, 0.020), (100.0, 150.0),
+             (0.010, 0.050), (100.0, 150.0), (5.000, 13.00), (10.00, 16.00),
+             (0.080, 0.120), (1.4e5, 1.9e5), (0.140, 0.220), (4.0e4, 8.5e4),
+             (0.020, 0.100), (80.00, 110.0), (0.080, 0.150), (80.00, 120.0),
+             (5.000, 13.00), (10.00, 16.00))
 
 
 def findline(text, parser):
@@ -30,7 +47,7 @@ def findline(text, parser):
     return index
 
 
-def getGaussianGradient(fname):
+def getGaussianEnergyGradient(fname):
     """
     Return xyz, gradient.
     """
@@ -44,6 +61,10 @@ def getGaussianGradient(fname):
     xyz = [[float(j) for j in i[-3:]] for i in data]
     xyz = unit.Quantity(value=np.array(xyz), unit=unit.angstrom)
 
+    eline = text[findline(text, "SCF Done:")[0]]
+    energy = unit.Quantity(value=float(eline.strip().split()[
+                           4]) * 627.5, unit=unit.kilocalorie_per_mole)
+
     start = findline(text, "Forces (Hartrees/Bohr)")[0] + 3
     end = findline(text, "Cartesian Forces")[0] - 1
     data = text[start:end]
@@ -52,10 +73,10 @@ def getGaussianGradient(fname):
     grad = unit.Quantity(value=np.array(
         grad), unit=unit.kilocalorie_per_mole / unit.bohr)
 
-    return xyz, grad
+    return xyz, energy, grad
 
 
-def genScoreFunc(xyzs, grads, template):
+def genTotalScore(xyzs, eners, grads, template, state_templates=[]):
     """
     Generate score func.
     """
@@ -63,22 +84,34 @@ def genScoreFunc(xyzs, grads, template):
         """
         Return score::float
         """
+        # gen state files
+        for name, temp in state_templates:
+            with open("%s.xml" % name, "w") as f:
+                f.write(temp.render(var=np.abs(var)))
         # gen config file
         conf = json.loads(template.render(var=var))
         # gen halmitonian
         H = evb.EVBHamiltonian(conf)
         # calc forces
-        error = []
-        try:
-            for n, xyz in enumerate(xyzs):
-                energy, gradient = H.calcEnergyGrad(xyz)
-                error.append(gradient - grads[n])
-            # compare
-            error = [np.abs(i.value_in_unit(
-                unit.kilojoule_per_mole / unit.angstrom)) for i in error]
-            return sum(error).sum() / len(error) / error[0].shape[0] / error[0].shape[1]
-        except:
-            return 1e5
+        calc_ener, calc_grad = [], []
+        for n, xyz in enumerate(xyzs):
+            energy, gradient = H.calcEnergyGrad(xyz)
+            calc_ener.append(energy)
+            calc_grad.append(gradient)
+        # compare
+        calc_ener = np.array(
+            [i.value_in_unit(unit.kilojoule / unit.mole) for i in calc_ener])
+        ref_ener = np.array(
+            [i.value_in_unit(unit.kilojoule / unit.mole) for i in eners])
+        var_ener = np.sqrt(
+            (np.abs((calc_ener - calc_ener.max()) - (ref_ener - ref_ener.max())) ** 2).sum())
+
+        calc_grad = np.array([i.value_in_unit(
+            unit.kilojoule_per_mole / unit.angstrom) for i in calc_grad]).ravel()
+        ref_grad = np.array([i.value_in_unit(
+            unit.kilojoule_per_mole / unit.angstrom) for i in grads]).ravel()
+        var_grad = np.sqrt(((calc_grad - ref_grad) ** 2).mean())
+        return var_grad + var_ener
     return valid
 
 
@@ -87,44 +120,57 @@ def combine(a, b, T=0.1):
     for i in range(a.shape[0]):
         res[i] = a[i] if np.random.random() > 0.5 else b[i]
         if np.random.random() < T:
-            res[i] += (np.random.random() * 2 - 1) * 10.0
+            res[i] = res[i] * (1 + (np.random.random() - 0.5)
+                               * 2 * 0.2)
     return res
 
 
 def randomGen(shape):
-    return (np.random.random(shape) - 0.5) * 2e3
+    res = np.zeros(VAR_INIT.shape)
+    for n in range(res.shape[0]):
+        imin, imax = VAR_LIMIT[n]
+        res[n] = np.random.random() * (imax - imin) + imin
+    return res
 
 
 def main():
     fnames = os.listdir(QMDATA)
-    xyzs, grads = [], []
+    xyzs, eners, grads = [], [], []
     for fname in fnames:
-        txyz, tgrad = getGaussianGradient(QMDATA + fname)
+        txyz, tener, tgrad = getGaussianEnergyGradient(QMDATA + fname)
         xyzs.append(txyz)
+        eners.append(tener)
         grads.append(tgrad)
 
     with open(TEMPFILE, "r") as f:
         template = Template("".join(f))
 
-    validfunc = genScoreFunc(xyzs, grads, template)
+    state_templates = []
+    for fname in STATE_TEMPFILE:
+        with open(fname, "r") as f:
+            state_templates.append([fname.split(".")[0], Template("".join(f))])
 
-    var_set = [randomGen((VAR_SHAPE,)) for _ in range(INITSIZE)]
+    validfunc = genTotalScore(
+        xyzs, eners, grads, template, state_templates=state_templates)
+
+    var_set = [randomGen(VAR_INIT.shape) for _ in range(INITSIZE)]
     while True:
-        score_set = [[v,validfunc(v)] for v in var_set]
-        rem = sorted(score_set, key=lambda v:v[1])[:REMAIN]
+        score_set = [[v, validfunc(v)] for v in var_set]
+        rem = sorted(score_set, key=lambda v: v[1])[:REMAIN]
         print("E:", rem[0][1])
         print("PARAM:")
-        print(rem[0][0],"\n")
+        print(rem[0][0], "\n")
         var_set = []
+        for i, s in rem[:TOP]:
+            var_set.append(i)
         for _ in range(LOWMULT):
-            ri, rj = np.random.randint(0,REMAIN), np.random.randint(0,REMAIN)
-            var_set.append(combine(rem[ri][0],rem[rj][0], T=0.1))
+            ri, rj = np.random.randint(0, REMAIN), np.random.randint(0, REMAIN)
+            var_set.append(combine(rem[ri][0], rem[rj][0], T=0.1))
         for _ in range(HIGHMULT):
-            ri, rj = np.random.randint(0,REMAIN), np.random.randint(0,REMAIN)
-            var_set.append(combine(rem[ri][0],rem[rj][0], T=0.25))
+            ri, rj = np.random.randint(0, REMAIN), np.random.randint(0, REMAIN)
+            var_set.append(combine(rem[ri][0], rem[rj][0], T=0.25))
         for _ in range(NEWGEN):
             var_set.append(randomGen(var_set[0].shape))
-
 
 
 if __name__ == '__main__':
