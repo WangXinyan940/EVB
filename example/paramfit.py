a258dba6b1465a5418107ca5a32dd2c1d19e5b41
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-01-25 15:49:56 +0800

    Fit params in both .json and .xml

--- example/paramfit.py
+++ example/paramfit.py
@@ -12,7 +12,14 @@ import matplotlib.pyplot as plt
 
 QMDATA = "qm/"
 TEMPFILE = "conf.temp"
-VAR = np.array([-36.61,  0.9652,  0.7561, -0.2479, 3.4120,  0.4087])
+STATE_TEMPFILE = ["state_1.temp", "state_2.temp"]
+VAR = np.array([-35.000,  0.5870,  0.5664, -0.0232886395,
+                 2.4108,  1.7586,  0.10749,  167845.344,
+                 0.1813,  77152.96,  0.010051,  115.705145,
+                 0.0283,  112.2809,  8.359835,  13.6440055,
+                 0.1076,  162364.304,  0.1900658,  59835.384,
+                 0.0624,  94.8828612,  0.1157697,  99.824709,
+                 8.4451,  13.6449845])
 
 
 def findline(text, parser):
@@ -52,7 +59,7 @@ def getGaussianEnergyGradient(fname):
     return xyz, energy, grad
 
 
-def genGradScore(xyzs, grads, template):
+def genGradScore(xyzs, grads, template, state_templates=[]):
     """
     Generate score func.
     """
@@ -60,6 +67,9 @@ def genGradScore(xyzs, grads, template):
         """
         Return score::float
         """
+        for name, temp in state_templates:
+            with open("%s.xml"%name, "w") as f:
+                f.write(temp.render(var=np.abs(var)))
         # gen config file
         conf = json.loads(template.render(var=var))
         # gen halmitonian
@@ -79,7 +89,7 @@ def genGradScore(xyzs, grads, template):
     return valid
 
 
-def genEnergyScore(xyzs, ener, template):
+def genEnergyScore(xyzs, ener, template, state_templates=[]):
     """
     Generate score func.
     """
@@ -87,6 +97,9 @@ def genEnergyScore(xyzs, ener, template):
         """
         Return score::float
         """
+        for name, temp in state_templates:
+            with open("%s.xml"%name, "w") as f:
+                f.write(temp.render(var=np.abs(var)))
         # gen config file
         conf = json.loads(template.render(var=var))
         # gen halmitonian
@@ -105,7 +118,7 @@ def genEnergyScore(xyzs, ener, template):
     return valid
 
 
-def genTotalScore(xyzs, eners, grads, template):
+def genTotalScore(xyzs, eners, grads, template, state_templates=[]):
     """
     Generate score func.
     """
@@ -113,6 +126,10 @@ def genTotalScore(xyzs, eners, grads, template):
         """
         Return score::float
         """
+        # gen state files
+        for name, temp in state_templates:
+            with open("%s.xml"%name, "w") as f:
+                f.write(temp.render(var=np.abs(var)))
         # gen config file
         conf = json.loads(template.render(var=var))
         # gen halmitonian
@@ -139,7 +156,13 @@ def genTotalScore(xyzs, eners, grads, template):
         return var_grad + var_ener
     return valid
 
-def drawPicture(xyzs, eners, grads, var, template):
+
+def drawPicture(xyzs, eners, grads, var, template, state_templates=[]):
+
+    for name, temp in state_templates:
+        with open("%s.xml"%name, "w") as f:
+            f.write(temp.render(var=np.abs(var)))
+
     conf = json.loads(template.render(var=var))
     H = evb.EVBHamiltonian(conf)
     calc_ener, calc_grad = [], []
@@ -147,38 +170,39 @@ def drawPicture(xyzs, eners, grads, var, template):
         energy, gradient = H.calcEnergyGrad(xyz)
         calc_ener.append(energy)
         calc_grad.append(gradient)
-    calc_ener = np.array([i.value_in_unit(unit.kilojoule / unit.mole) for i in calc_ener])
+    calc_ener = np.array(
+        [i.value_in_unit(unit.kilojoule / unit.mole) for i in calc_ener])
     calc_ener = calc_ener - calc_ener.max()
-    ref_ener = np.array([i.value_in_unit(unit.kilojoule / unit.mole) for i in eners])
+    ref_ener = np.array(
+        [i.value_in_unit(unit.kilojoule / unit.mole) for i in eners])
     ref_ener = ref_ener - ref_ener.max()
-    plt.plot([calc_ener.min(),calc_ener.max()],[calc_ener.min(),calc_ener.max()],c='black')
-    plt.scatter(calc_ener, ref_ener,c="red")
+    plt.plot([calc_ener.min(), calc_ener.max()], [
+             calc_ener.min(), calc_ener.max()], c='black')
+    plt.scatter(calc_ener, ref_ener, c="red")
     plt.xlabel("CALC ENERGY")
     plt.ylabel("REF ENERGY")
     #plt.plot(calc_ener, c="red")
     #plt.plot(ref_ener, c="black")
-    #plt.xlabel("Sample")
+    # plt.xlabel("Sample")
     #plt.ylabel("Energy (kJ/mol)")
     plt.show()
-
-    calc_grad = np.array([i.value_in_unit(unit.kilojoule_per_mole / unit.angstrom).ravel() for i in calc_grad]).ravel()
-    ref_grad = np.array([i.value_in_unit(unit.kilojoule_per_mole / unit.angstrom).ravel() for i in grads]).ravel()
-    plt.plot([calc_grad.min(),calc_grad.max()],[calc_grad.min(),calc_grad.max()])
-    plt.scatter(calc_grad, ref_grad)
+    cmap = ["k", "r", "y", "g", "b", "m"]
+    calc_grad = np.array(
+        [i.value_in_unit(unit.kilojoule_per_mole / unit.angstrom) for i in calc_grad])
+    ref_grad = np.array(
+        [i.value_in_unit(unit.kilojoule_per_mole / unit.angstrom) for i in grads])
+    plt.plot([calc_grad.min(), calc_grad.max()],
+             [calc_grad.min(), calc_grad.max()])
+    for n in range(calc_grad.shape[1]):
+        plt.scatter(calc_grad[:, n, :].ravel(), ref_grad[
+                    :, n, :].ravel(), c=cmap[n], label="%s" % n)
+    plt.legend()
     plt.xlabel("CALC GRADIENT")
     plt.ylabel("REF GRADIENT")
     plt.show()
 
 
-
-def main():
-    """
-    Main function to parameterize.
-    """
-    global QMDATA
-    global TEMPFILE
-    global VAR
-
+if __name__ == '__main__':
     fnames = os.listdir(QMDATA)
     xyzs, eners, grads = [], [], []
     for fname in fnames:
@@ -190,16 +214,19 @@ def main():
     with open(TEMPFILE, "r") as f:
         template = Template("".join(f))
 
+    state_templates = []
+    for fname in STATE_TEMPFILE:
+        with open(fname, "r") as f:
+            state_templates.append([fname.split(".")[0], Template("".join(f))])
+
     efunc = genEnergyScore(xyzs, eners, template)
     gfunc = genGradScore(xyzs, grads, template)
-    tfunc = genTotalScore(xyzs, eners, grads, template)
-    min_result = optimize.minimize(gfunc, VAR, jac="2-point", hess="2-point", method='L-BFGS-B', options=dict(maxiter=1, disp=True, gtol=0.001))
+    tfunc = genTotalScore(xyzs, eners, grads, template, state_templates=state_templates)
+    drawPicture(xyzs, eners, grads, VAR, template, state_templates=state_templates)
+    def print_func(x, f, accepted):
+        print("at minimum %.4f accepted %d"%(f, int(accepted)))
+    min_result = optimize.basinhopping(tfunc, VAR, minimizer_kwargs={"method":"L-BFGS-B", "jac":"2-point"}, niter=1, callback=print_func)
+    #min_result = optimize.minimize(tfunc, VAR, jac="2-point", hess="2-point", method='L-BFGS-B', options=dict(maxiter=1000, disp=True, gtol=0.0001))
     print(min_result)
-
-    drawPicture(xyzs, eners, grads, min_result.x, template)
-
-
-
-
-if __name__ == '__main__':
-    main()
+    
+    drawPicture(xyzs, eners, grads, min_result.x, template, state_templates=state_templates)
