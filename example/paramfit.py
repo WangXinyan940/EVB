21954c995d8dd73291cd8804a961e30d7025e14e
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-01-29 15:40:52 +0800

    Update paramfit.py

--- example/paramfit.py
+++ example/paramfit.py
@@ -14,14 +14,13 @@ from tempfile import TemporaryDirectory
 QMDATA = "qm/"
 TEMPFILE = "conf.temp"
 STATE_TEMPFILE = ["state_1.temp", "state_2.temp"]
-VAR = np.array([-8.82047150e+00,  4.38666042e-01,  2.39817401e-01, -8.46593935e-03,
-                4.01405058e+00,  9.18157661e-01,  1.07899640e-01,  4.32995384e+04,
-                1.83148456e-01,  1.10367501e+05,  1.62516059e-02,  8.51126256e+01,
-                4.77436275e-02,  1.02434555e+02,  6.61435431e+00,  2.01263896e+00, 2.01263896e+00,
-                1.06777298e-01, -9.54761776e+03,  2.01137910e-01,  6.22241691e+04,
-                2.84185939e-02,  8.40873651e+01,  6.95016903e-02,  9.84080912e+01,
-                1.13992471e+01,  7.67094349e+00,  2.01263896e+00])
-
+VAR = np.array([-1.02178714e+01,  2.96289306e-01,  1.64063455e-01,  1.16490327e-02,
+                 4.81039490e+00,  5.57221210e-01,  1.07854875e-01,  3.67539345e+04,
+                 1.83496498e-01,  8.32166788e+04,  1.22943053e-02,  1.11986160e+02,
+                 1.79091272e-02,  1.05608702e+02,  5.25430179e+00,  1.77521684e+00,
+                 2.71316766e+00,  1.13375097e-01, -9.53444603e+03,  1.98967448e-01,
+                 5.73826376e+04,  1.57761737e-02,  8.59267473e+01,  3.47319534e-02,
+                 9.42591073e+01,  5.79449948e-03,  1.78091636e+00,  2.05894451e+00,])
 
 TEMPDIR = TemporaryDirectory()
 
@@ -134,9 +133,9 @@ def genTotalScore(xyzs, eners, grads, template, state_templates=[]):
         try:
             for name, temp in state_templates:
                 with open("%s/%s.xml" % (TEMPDIR.name, name), "w") as f:
-                    f.write(temp.render(var=np.abs(VAR * (1 + var / 100.0))))
+                    f.write(temp.render(var=np.abs(var)))
             # gen config file
-            conf = json.loads(template.render(var=VAR * (1 + var / 100.0)))
+            conf = json.loads(template.render(var=var))
             for n, fn in enumerate(state_templates):
                 conf["diag"][n][
                     "parameter"] = "%s/%s.xml" % (TEMPDIR.name, fn[0])
@@ -172,9 +171,9 @@ def drawPicture(xyzs, eners, grads, var, template, state_templates=[]):
 
     for name, temp in state_templates:
         with open("%s/%s.xml" % (TEMPDIR.name, name), "w") as f:
-            f.write(temp.render(var=np.abs(VAR * (1 + var / 100.0))))
+            f.write(temp.render(var=np.abs(var)))
 
-    conf = json.loads(template.render(var=VAR * (1 + var / 100.0)))
+    conf = json.loads(template.render(var=var))
     for n, fn in enumerate(state_templates):
         conf["diag"][n]["parameter"] = "%s/%s.xml" % (TEMPDIR.name, fn[0])
     H = evb.EVBHamiltonian(conf)
@@ -217,7 +216,7 @@ def drawPicture(xyzs, eners, grads, var, template, state_templates=[]):
 
 def basinhopping(score, var, niter=20, bounds=None, T=1.0, pert=7.0):
     newvar = np.zeros(var.shape)
-    newvar[:] = var[:] + (np.random.random(var.shape) * 2 - 1.0) * pert
+    newvar[:] = var[:]
     posvar = np.zeros(var.shape)
     posvar[:] = newvar[:]
     posscore = np.inf
@@ -225,7 +224,7 @@ def basinhopping(score, var, niter=20, bounds=None, T=1.0, pert=7.0):
     for ni in range(niter):
         print("\nRound %i. Start BFGS." % ni)
         min_result = optimize.minimize(score, newvar, jac="2-point", hess="2-point",
-                                       method='L-BFGS-B', options=dict(maxiter=500, disp=True, gtol=0.01))
+                                       method='L-BFGS-B', options=dict(maxiter=500, disp=True, gtol=0.1))
         print("Result:")
         print(min_result.x)
         print("")
@@ -294,10 +293,10 @@ if __name__ == '__main__':
             tmin = bool(np.all(x >= self.xmin))
             return tmax and tmin
 
-    mybounds = MyBounds(xmax=[100.0 for i in VAR], xmin=[-100.0 for i in VAR])
+    mybounds = MyBounds(xmax=100 * VAR, xmin=- VAR)
 
-    traj = basinhopping(tfunc, np.zeros(VAR.shape), niter=50,
-                        bounds=mybounds, T=2.0, pert=25.0)
+    traj = basinhopping(tfunc, VAR, niter=50,
+                        bounds=mybounds, T=2.0, pert=5.0)
     #min_result = optimize.minimize(tfunc, VAR, jac="2-point", hess="2-point", method='L-BFGS-B', options=dict(maxiter=1000, disp=True, gtol=0.0001))
 
     drawPicture(xyzs, eners, grads, traj[0][1],
