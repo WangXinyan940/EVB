f77b6bd3af5801d672920a4e796652cf757f89c2
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-01-31 10:13:18 +0800

    Update

--- example/sum.py
+++ example/sum.py
@@ -7,13 +7,13 @@ QMDATA = "qm/"
 HESSFILE = "hess/ts.log"
 TEMPFILE = "conf.temp"
 STATE_TEMPFILE = ["state_1.temp", "state_2.temp"]
-VAR = np.array([-1.02178714e+01,  2.96289306e-01,  1.64063455e-01,  1.16490327e-02,
-                 4.81039490e+00,  5.57221210e-01,  1.07854875e-01,  3.67539345e+04,
-                 1.83496498e-01,  8.32166788e+04,  1.22943053e-02,  1.11986160e+02,
-                 1.79091272e-02,  1.05608702e+02,  5.25430179e+00,  1.77521684e+00,
-                 2.71316766e+00,  1.13375097e-01, -9.53444603e+03,  1.98967448e-01,
-                 5.73826376e+04,  1.57761737e-02,  8.59267473e+01,  3.47319534e-02,
-                 9.42591073e+01,  5.79449948e-03,  1.78091636e+00,  2.05894451e+00, ])
+VAR = np.array([ 0.00000000e+00, -1.02178714e+01,  2.96289306e-01,  1.64063455e-01,  
+                 1.16490327e-02,  4.81039490e+00,  5.57221210e-01,  1.08700000e-01,
+                 1.67845344e+05,  1.77920000e-01,  7.71529600e+04,  3.81845978e-02,
+                 1.15710000e+02,  5.63082620e-02,  1.12320000e+02,  8.39782623e+00, 
+                 13.655595693963415, 0.10900000000000001, 162364.304, 0.19563, 
+                 59835.3840, 0.09128107114810141, 94.99, 0.14908052069524466, 
+                 100.33, 8.397826228907983, 13.655595693963415])
 
 
 def main():
@@ -40,7 +40,7 @@ def main():
                          state_templates=state_templates, a_diag=1.0, a_offdiag=1.00)
     tfunc = genEnerGradScore(xyzs, eners, grads, template,
                              state_templates=state_templates)
-    score = lambda x: 0.5 * hfunc(x) + tfunc(x)
+    score = lambda x: hfunc(x) + tfunc(x)
 
     drawEnergy(xyzs, eners, VAR, template, state_templates=state_templates)
     drawGradient(xyzs, grads, VAR, template, state_templates=state_templates)
