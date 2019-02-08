3eb7a41da53daa4030c3b15b9bd428ccefd0a170
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-02-08 15:23:15 +0800

    Update test.py

--- example2/test.py
+++ example2/test.py
@@ -1,6 +1,7 @@
 import multifit
 import json
 import numpy as np
+import os
 
 TEMPFILE = "conf.temp"
 VAR = np.array([ 1.00000000e+00,  1.00000000e+00,  1.00000000e+00,  0.00000000e+00,
@@ -14,18 +15,23 @@ HESSFILE = "freq.fchk"
 xyz, hess = getCHKHess(HESSFILE)
 mass = getCHKMass(HESSFILE)
 
+xyzs, eners, grads = [], [], []
+FORCEDIR = "force/"
+for fname in os.listdir(FORCEDIR):
+    if fname.split(".")[-1] == "log":
+        xyz, energy, grad = getGaussEnergyGradient(FORCEDIR+fname)
+        xyzs.append(xyz)
+        eners.append(energy)
+        grads.append(grad)
+
 with open(TEMPFILE, "r") as f:
     template = Template("".join(f))
 
 conf = json.loads(template.render(var=VAR))
 
-score = multifit.multigenHessScore(xyz, hess, mass, template, [5000, 5001, 5002, 5003])
-s, q = score(VAR)
+score = multifit.multigenEnerGradScore(xyzs, eners, grads, template, [5000, 5001, 5002, 5003])
+s = score(VAR)
 
-score2 = genHessScore(xyz, hess, mass, template)
-s2, q2 = score2(VAR)
-
+score2 = genEnerGradScore(xyzs, eners, grads, template)
+s2 = score2(VAR)
 print(s, s2)
-
-f = plt.scatter(q,q2)
-plt.show()
