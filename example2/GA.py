29aaaec5f481520636ef2cd4a87c73ffa4503fc7
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-02-10 22:42:45 +0800

    Bug fix

--- example2/GA.py
+++ example2/GA.py
@@ -12,9 +12,10 @@ VAR = np.array([365.1560798272886,  -370.9091564905557,  23.7498531682483,  -5.5
                 23.986752808835867,  48.63218346604885,  -115.93874045209743,  35.274995414925925,
                 54.723615215648444,  -81.82033046918013,  46.382985481108726])
 portlist = [i for i in range(5000,5017)]
-NSAMPLE = 200
-REMAIN = 90
-NROUND = 500
+INIT = 1000
+NSAMPLE = 360
+REMAIN = 250
+NROUND = 200
 
 def mult(v1, v2):
     res = np.zeros(v1.shape)
@@ -26,7 +27,13 @@ def mult(v1, v2):
     return res
 
 def ran(v, r = 5.0):
-    return v + (np.random.random(v.shape) - 0.5) * 2. * r
+    newv = np.zeros(v.shape)
+    for i in range(v.shape[0]):
+        if np.random.random() < 0.2:
+            newv[i] = v[i] + (np.random.random() - 0.5) * 2. * r
+        else:
+            newv[i] = v[i]
+    return newv
 
 
 def main():
@@ -58,15 +65,20 @@ def main():
 #                state_templates=state_templates)
 #    multidrawHess(xyz, hess, mass, VAR, template, portlist, state_templates=state_templates)
     
-    RES = [(np.random.random(VAR.shape) * 2. - 1.) * 400.0 for _ in range(NSAMPLE)]
+    RES = [(np.random.random(VAR.shape) * 2. - 1.) * 200.0 for _ in range(INIT)]
     for ni in range(NROUND):
         logging.info("Round {}".format(ni))
         score_list = sorted([[gfunc(i),i] for i in RES], key=lambda x:x[0])
         logging.info("min f: {}  min var: {}".format(score_list[0][0], " ".join("{}".format(_) for _ in score_list[0][1])))
-        RES = [i[1] for i in score_list[:REMAIN]]
+        RES = [i[1] for i in score_list[:REMAIN] if i[0] < 1e8]
+        tmp = len(RES)
+        for _ in range(tmp, REMAIN):
+            RES.append((np.random.random(VAR.shape) * 2. - 1.) * 200.0)
         for j in range(len(RES), NSAMPLE):
-            RES.append(ran(mult(np.random.choice(score_list[:REMAIN]),np.random.choice(score_list[:REMAIN]))))
-    logging.info("Finish. min f: {}  min var: {}".format(score_list[0][0], " ".join("{}".format(_) for _ in score_list[0][1])))
+            pi,pj = np.random.randint(0,tmp), np.random.randint(0,tmp)
+            RES.append(mult(score_list[pi][1],score_list[pj][1]))
+            RES = [ran(i) for i in RES]
+    logging.info("Finish. min f: {}  min var: {}".format(score_list[0][0], ", ".join("{}".format(_) for _ in score_list[0][1])))
     print(score_list[0][0])
     print(score_list[0][1])
     multidrawHess(xyz, hess, mass, score_list[0][1], template, portlist,
@@ -84,6 +96,7 @@ if __name__ == '__main__':
     ch.setLevel(logging.DEBUG)
     ch.setFormatter(formatter)
     fh = logging.FileHandler(sys.argv[1])
+    fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     logger.addHandler(ch)
     logger.addHandler(fh)
--- example2/multifit.py
+++ example2/multifit.py
@@ -14,6 +14,10 @@ from gevent.lock import BoundedSemaphore
 sem = BoundedSemaphore(200)
 
 
+
+class ParamFail(BaseException):
+    pass
+
 class EVBServer(object):
 
     def __init__(self, port):
@@ -68,6 +72,7 @@ class EVBServer(object):
                     sock.send("ERROR".encode("utf-8"))
             except Exception as e:
                 logging.error("COLLAPSE: " + str(e))
+                sock.send("ERROR".encode("utf-8"))
             finally:
                 sock.close()
 
@@ -102,6 +107,7 @@ class EVBClient(object):
         ifok = np.zeros((len(self.port_list),))
         def init(pt, n, ifok):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(10)
             s.connect(("127.0.0.1", pt))
             data = "INIT" + json.dumps(conf)
             s.send(data.encode("utf-8"))
@@ -122,13 +128,15 @@ class EVBClient(object):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         pt = self.port_list[self.pi % len(self.port_list)]
         self.pi += 1
+        s.settimeout(10)
         s.connect(("127.0.0.1", pt))
         data = "ENER" + " ".join("%18.10f" % i for i in xyz_no_unit.ravel())
         s.send(data.encode("utf-8"))
         ret = s.recv(1024).decode("utf-8")
         if ret == "ERROR":
             ans = 1
-            return ans, ans
+            s.close()
+            raise ParamFail("Value NaNs")
         else:
             ans = 0
         s.close()
@@ -139,25 +147,31 @@ class EVBClient(object):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         pt = self.port_list[self.pi % len(self.port_list)]
         self.pi += 1
+        s.settimeout(10)
         s.connect(("127.0.0.1", pt))
-        data = "GRAD" + " ".join("%18.10f" % i for i in xyz_no_unit.ravel())
-        s.send(data.encode("utf-8"))
-        buff = []
-        while True:
-            d = s.recv(1024)
-            buff.append(d)
-            if len(d) < 1024:
-                break
-        ret = b"".join(buff).decode("utf-8")
-        if ret == "ERROR":
-            ans = 1
-            return ans, ans, ans
-        else:
-            ans = 0
-        s.close()
-        ret = np.array([float(i) for i in ret.strip().split()])
-        return ans, unit.Quantity(ret[0], unit.kilojoule_per_mole), unit.Quantity(ret[1:].reshape((-1, 3)), unit.kilojoule_per_mole / unit.angstrom)
-
+        try:
+            data = "GRAD" + " ".join("%18.10f" % i for i in xyz_no_unit.ravel())
+            s.send(data.encode("utf-8"))
+            buff = []
+            while True:
+                d = s.recv(1024)
+                buff.append(d)
+                if len(d) < 1024:
+                    break
+            ret = b"".join(buff).decode("utf-8")
+            if ret == "ERROR":
+                ans = 1
+                s.close()
+                raise ParamFail("Value NaNs")
+            else:
+                ans = 0
+            s.close()
+            ret = np.array([float(i) for i in ret.strip().split()])
+            return ans, unit.Quantity(ret[0], unit.kilojoule_per_mole), unit.Quantity(ret[1:].reshape((-1, 3)), unit.kilojoule_per_mole / unit.angstrom)
+        except BaseException as e:
+            logging.debug("Client error " + str(e))
+            s.close()
+            raise e
 
 def multigenHessScore(xyz, hess, mass, template, portlist, state_templates=[], dx=0.00001, a_diag=1.00, a_offdiag=1.00):
     """
@@ -176,74 +190,85 @@ def multigenHessScore(xyz, hess, mass, template, portlist, state_templates=[], d
         Return score::float
         """
         # gen state files
+        try:
+            for name, temp in state_templates:
+                with open("%s/%s.xml" % (TEMPDIR.name, name), "w") as f:
+                    f.write(temp.render(var=np.abs(var)))
+            # gen config file
+            conf = json.loads(template.render(var=var))
+            for n, fn in enumerate(state_templates):
+                conf["diag"][n]["parameter"] = "%s/%s.xml" % (TEMPDIR.name, fn[0])
+            # gen halmitonian
+            client = EVBClient(portlist)
+            ret = client.initialize(conf)
+            # calc hess (unit in kJ / mol / A^2)
+            oxyz = xyz.value_in_unit(unit.angstrom).ravel()
+            dxyz = np.eye(oxyz.shape[0])
+            calc_hess = np.zeros(dxyz.shape)
 
-        for name, temp in state_templates:
-            with open("%s/%s.xml" % (TEMPDIR.name, name), "w") as f:
-                f.write(temp.render(var=np.abs(var)))
-        # gen config file
-        conf = json.loads(template.render(var=var))
-        for n, fn in enumerate(state_templates):
-            conf["diag"][n]["parameter"] = "%s/%s.xml" % (TEMPDIR.name, fn[0])
-        # gen halmitonian
-        client = EVBClient(portlist)
-        ret = client.initialize(conf)
-        # calc hess (unit in kJ / mol / A^2)
-        oxyz = xyz.value_in_unit(unit.angstrom).ravel()
-        dxyz = np.eye(oxyz.shape[0])
-        calc_hess = np.zeros(dxyz.shape)
+            def func(gi, hmat):
+                sem.acquire()
+                try:
+                    txyz = unit.Quantity(
+                        value=(oxyz + dxyz[:, gi] * dx).reshape((-1, 3)), unit=unit.angstrom)
+                    _, tep, tgp = client.calcEnergyGrad(txyz)
+                    if _ == 0:
+                        tgp = tgp.value_in_unit(
+                            unit.kilocalorie_per_mole / unit.angstrom).ravel()
+                # parallel
+                    txyz = unit.Quantity(
+                        value=(oxyz - dxyz[:, gi] * dx).reshape((-1, 3)), unit=unit.angstrom)
+                    _, ten, tgn = client.calcEnergyGrad(txyz)
+                    if _ == 0:
+                        tgn = tgn.value_in_unit(
+                            unit.kilocalorie_per_mole / unit.angstrom).ravel()
+                    hmat[:, gi] = (tgp - tgn) / 2.0 / dx
+                    sem.release()
+                    return 0
+                except ParamFail as e:
+                    sem.release()
+                    return 1
+                
 
-        def func(gi, hmat):
-            sem.acquire()
-            txyz = unit.Quantity(
-                value=(oxyz + dxyz[:, gi] * dx).reshape((-1, 3)), unit=unit.angstrom)
-            _, tep, tgp = client.calcEnergyGrad(txyz)
-            tgp = tgp.value_in_unit(
-                unit.kilocalorie_per_mole / unit.angstrom).ravel()
-        # parallel
-            txyz = unit.Quantity(
-                value=(oxyz - dxyz[:, gi] * dx).reshape((-1, 3)), unit=unit.angstrom)
-            _, ten, tgn = client.calcEnergyGrad(txyz)
-            tgn = tgn.value_in_unit(
-                unit.kilocalorie_per_mole / unit.angstrom).ravel()
-            hmat[:, gi] = (tgp - tgn) / 2.0 / dx
-            sem.release()
+            # for gi in range(dxyz.shape[0]):
+            #    func(gi, calc_hess)
 
-        # for gi in range(dxyz.shape[0]):
-        #    func(gi, calc_hess)
+            # for gi in range(dxyz.shape[0]):
+            #    pl.spawn(func(gi))
+            # pl.join()
 
-        # for gi in range(dxyz.shape[0]):
-        #    pl.spawn(func(gi))
-        # pl.join()
+            ret = gevent.joinall([gevent.spawn(func, gi, calc_hess)
+                            for gi in range(dxyz.shape[0])])
+            if sum([i.value for i in ret]) > 1e-5:
+                raise ParamFail("Emmmmmm")
+            calc_theta = np.dot(mass_mat, np.dot(calc_hess, mass_mat))
+            # change basis
+            calc_theta_p = np.dot(qvI, np.dot(calc_theta, qv))
 
-        gevent.joinall([gevent.spawn(func, gi, calc_hess)
-                        for gi in range(dxyz.shape[0])])
+            vib_qm, vib_mm = np.diag(theta_p), np.diag(calc_theta_p)
+            vib_qm = unit.Quantity(
+                vib_qm, unit.kilocalorie_per_mole / unit.angstrom ** 2 / unit.amu)
+            vib_mm = unit.Quantity(
+                vib_mm, unit.kilocalorie_per_mole / unit.angstrom ** 2 / unit.amu)
+            vib_qm = vib_qm.value_in_unit(
+                unit.joule / unit.meter ** 2 / unit.kilogram)
+            vib_mm = vib_mm.value_in_unit(
+                unit.joule / unit.meter ** 2 / unit.kilogram)
+            vib_qm = np.sqrt(np.abs(vib_qm)) / 2. / np.pi / \
+                2.99792458e10 * np.sign(vib_qm)
+            vib_mm = np.sqrt(np.abs(vib_mm)) / 2. / np.pi / \
+                2.99792458e10 * np.sign(vib_mm)
 
-        calc_theta = np.dot(mass_mat, np.dot(calc_hess, mass_mat))
-        # change basis
-        calc_theta_p = np.dot(qvI, np.dot(calc_theta, qv))
-
-        vib_qm, vib_mm = np.diag(theta_p), np.diag(calc_theta_p)
-        vib_qm = unit.Quantity(
-            vib_qm, unit.kilocalorie_per_mole / unit.angstrom ** 2 / unit.amu)
-        vib_mm = unit.Quantity(
-            vib_mm, unit.kilocalorie_per_mole / unit.angstrom ** 2 / unit.amu)
-        vib_qm = vib_qm.value_in_unit(
-            unit.joule / unit.meter ** 2 / unit.kilogram)
-        vib_mm = vib_mm.value_in_unit(
-            unit.joule / unit.meter ** 2 / unit.kilogram)
-        vib_qm = np.sqrt(np.abs(vib_qm)) / 2. / np.pi / \
-            2.99792458e10 * np.sign(vib_qm)
-        vib_mm = np.sqrt(np.abs(vib_mm)) / 2. / np.pi / \
-            2.99792458e10 * np.sign(vib_mm)
-
-        var = (calc_theta_p - theta_p) ** 2
-        s_qm = np.sort(np.abs(vib_qm))[6:]
-        s_mm = np.sort(np.abs(vib_mm))[6:]
-        var_diag = ((s_qm - s_mm) ** 2).sum() / s_mm.shape[0]
-        var_offdiag = (var - np.diag(np.diag(var))).sum() / \
-            (var.shape[0] ** 2 - var.shape[0])
-        return a_diag * var_diag + a_offdiag * var_offdiag
-
+            var = (calc_theta_p - theta_p) ** 2
+            s_qm = np.sort(np.abs(vib_qm))[6:]
+            s_mm = np.sort(np.abs(vib_mm))[6:]
+            var_diag = ((s_qm - s_mm) ** 2).sum() / s_mm.shape[0]
+            var_offdiag = (var - np.diag(np.diag(var))).sum() / \
+                (var.shape[0] ** 2 - var.shape[0])
+            return a_diag * var_diag + a_offdiag * var_offdiag
+        except ParamFail as e:
+            logging.debug(str(e))
+            return 1e12
     return valid
 
 
@@ -271,20 +296,26 @@ def multigenEnerGradScore(xyzs, eners, grads, template, portlist, state_template
             # calc forces
             calc_ener = np.zeros((len(xyzs),))
             calc_grad = np.zeros((len(xyzs), xyzs[0].ravel().shape[0]))
-            ifok = 0
 
-            def func(n, e_array, g_array, ifok):
+            def func(n, e_array, g_array):
                 sem.acquire()
-                _, energy, gradient = client.calcEnergyGrad(xyzs[n])
-                if _ == 0:
-                    e_array[n] = energy.value_in_unit(unit.kilojoule / unit.mole)
-                    g_array[n, :] = gradient.value_in_unit(
-                        unit.kilojoule_per_mole / unit.angstrom).ravel()
-                ifok += _
-                sem.release()
+                try:
+                    _, energy, gradient = client.calcEnergyGrad(xyzs[n])
+                    if _ == 0:
+                        e_array[n] = energy.value_in_unit(unit.kilojoule / unit.mole)
+                        g_array[n, :] = gradient.value_in_unit(
+                            unit.kilojoule_per_mole / unit.angstrom).ravel()
+                    sem.release()
+                    return 0
+                except BaseException as e:
+                    logging.debug("Parallel: " + str(e))
+                    sem.release()
+                    return 1
 
-            gevent.joinall([gevent.spawn(func, n, calc_ener, calc_grad, ifok)
+            ret = gevent.joinall([gevent.spawn(func, n, calc_ener, calc_grad)
                             for n in range(len(xyzs))])
+            if sum([i.value for i in ret if i.value is not None]) > 1e-5:
+                raise ParamFail("Emmmmmm")
             # compare
             ref_ener = np.array(
                 [i.value_in_unit(unit.kilojoule / unit.mole) for i in eners])
@@ -297,9 +328,9 @@ def multigenEnerGradScore(xyzs, eners, grads, template, portlist, state_template
 
             var_grad = np.sqrt(((calc_grad - ref_grad) ** 2).mean())
             return a_grad * var_grad + a_ener * var_ener
-        except Exception as e:
-            print(e)
-            return 100000.0
+        except ParamFail as e:
+            logging.debug("Score: " + str(e))
+            return 1e12
     return valid
 
 
