8e70a5d45374fe30c889b05c731af003d6988620
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-02-15 13:41:58 +0800

    Update press.py

--- example2/press.py
+++ example2/press.py
@@ -7,12 +7,11 @@ from simtk import unit
 
 with open("conf.temp", "r") as f:
     temp = Template("".join(f))
-conf = json.loads(temp.render(var=np.zeros((15,))))
-portlist = [5000, 5000, 5000, 5000]
-
+portlist = [9000, 9000, 9000, 9000]
 xyz, hess = getCHKHess("freq.fchk")
 
 while True:
+    conf = json.loads(temp.render(var=np.random.random((15,))))
     client = EVBClient(portlist)
     client.initialize(conf)
     _ = client.calcEnergyGrad(xyz)
